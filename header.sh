f7212c202743bfdb0b92783ae5502b0d02bc4df8
Tiago SM <teagom@gmail.com>
2023-02-09 15:32:34 -0300

    Start Robo13 Bash Backup Script. Initial code and files.

---
+++ header.sh
@@ -0,0 +1,9 @@
+START=$(date)
+echo
+echo "= Head :::::::::::::::::::::::::::::::::::::::::::::"
+echo "  Script     : $0"
+echo "  Start      : $START"
+echo "  Section    : $SECTION"
+echo "  Destination: $DESTINATION"
+echo "  Format     : $FORMAT"
+echo "  Backup     : $BACKUP"
---
+++ resume.sh
@@ -0,0 +1,9 @@
+FINISH=$(date)
+echo
+echo "= Resume :::::::::::::::::::::::::::::::::::::::::::::"
+echo "  Script     : $0"
+echo "  Start      : $START"
+echo "  Finish     : $FINISH"
+echo "  Destination: $DESTINATION"
+echo "  Backup     : $BACKUP"
+echo "  Total Size : $SIZE"
